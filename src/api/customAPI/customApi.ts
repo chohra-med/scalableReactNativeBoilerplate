d4ba55b42eaae4c3a55ebc771d78083ea901fe15
malik chohra <malik.chohra@docmorris.de>
2024-10-18 16:51:22 +0200

    add react native config

--- src/api/customAPI/customApi.ts
+++ src/api/customAPI/customApi.ts
@@ -1,7 +1,8 @@
 import { customApiAxios } from '../Api';
 import { ROUTE_CUSTOM_ROUTE } from './routes';
-
+import { Config } from 'react-native-config'
 class CustomAPI {
+  appToken = Config.APP_TOKEN;
   /*get the list of the customs */
 
   LoadAllCustoms() {
